569c3a40cf4d8e2d9cb6ccdb93ad84010e6ee086
Demian Voksi <demian.voksi@gmail.com>
2021-07-07 01:09:52 +0200

    count and multiply

---
+++ kata/8_kyu/count_by_x.js
@@ -0,0 +1,24 @@
+// 8 kyu
+
+// Javascript
+
+// Count by X
+
+// By: shums16
+
+// Create a function with two arguments that will return an array
+// of the first (n) multiples of (x).
+// Assume both the given number and the number of times to count
+// will be positive numbers greater than 0.
+// Return the results as an array.
+
+function countBy(x, n) {
+    var z = [];
+    const neo = x;
+    z.push(neo);
+    for (var i = 0; i < (n - 1); i++) {
+	x = x + neo;
+	z.push(x);
+    }
+    return z;
+}
---
+++ kata/8_kyu/multiply.js
@@ -0,0 +1,14 @@
+// 8 kyu
+
+// Multiply
+
+// Javascript
+
+// By: ?
+
+// This code does not execute properly. Try to figure out why.
+
+function multiply(a, b) {
+  var c = a * b;
+  return c;
+}
