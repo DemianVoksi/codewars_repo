c578167ae9d27c5ecb5792454308b364289f744b
Demian Voksi <demian.voksi@gmail.com>
2021-07-06 19:48:53 +0200

    8_kyu JS

---
+++ kata/8_kyu/convert_number_to_reversed.js
@@ -0,0 +1,19 @@
+// 8 kyu
+
+// Javascript
+
+// Convert number to reversed array of digits
+
+// by: emporio
+
+// Given a random non-negative number, you have to return
+// the digits of this number within an array in reverse order.
+
+function digitize(n) {
+    n2 = [];
+    let nn = Array.from(n.toString()).map(Number);
+    for (let i = 0; i < nn.length; i++) {
+    n2.unshift(nn[i]);
+    }
+    return n2;
+}
---
+++ kata/8_kyu/wide_mouth_frog.js
@@ -0,0 +1,19 @@
+// 8 kyu
+// Javascript
+// The Wide-Mouthed frog!
+// By: nbeck
+
+// Your goal in this kata is to create complete the mouth_size method
+// this method take one argument animal which corresponds to the animal
+// encountered by frog. If this one is an alligator (case insensitive)
+// return small otherwise return wide.
+
+function mouthSize(animal) {
+    if (animal == "alligator" || animal == "ALLIGATOR")
+	return "small";
+    else
+	return "wide";
+}
+
+console.log(mouthSize("alligator"));
+console.log(mouthSize("dog"));
