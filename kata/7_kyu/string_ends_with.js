abbf0072d7e1a81739caa8b27c779d5e9e29c205
Demian Voksi <demian.voksi@gmail.com>
2021-09-04 02:05:41 +0200

    string ends with ?

---
+++ kata/7_kyu/string_ends_with.js
@@ -0,0 +1,20 @@
+/*
+7 kyu
+Javascript
+String ends with?
+By: jhoffner
+
+Complete the solution so that it returns true if the first
+argument(string) passed in ends with the 2nd argument 
+(also a string). 
+*/
+
+function solution(str, ending){
+  if (str.slice(-ending.length) == ending) {
+    return true;
+  } else if (ending == '') {
+    return true;
+  } else {
+    return false;
+  }
+}
