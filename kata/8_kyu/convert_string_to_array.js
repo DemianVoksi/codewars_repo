87155524119de179263e8c48c26414dc3d410005
Demian Voksi <demian.voksi@gmail.com>
2021-10-25 21:55:04 +0200

    string to array js

---
+++ kata/8_kyu/convert_string_to_array.js
@@ -0,0 +1,13 @@
+/*
+8 Kyu
+Javascript
+Convert a string to an array
+By: samjam48
+
+Write a function to split a string and convert it 
+into an array of words. */
+
+function stringToArray(string){
+    let arr = string.split(" ");
+    return arr;
+}
