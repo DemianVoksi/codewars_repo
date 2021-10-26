95130e3356664cc0eb7c11ce2abbeab54d3aaa99
Demian Voksi <demian.voksi@gmail.com>
2021-10-27 00:51:30 +0200

    8 kyu

---
+++ kata/8_kyu/reverse_list_order.js
@@ -0,0 +1,13 @@
+/*
+Reverse List Order
+8 Kyu
+Javascript
+By: cmgerber
+
+In this kata you will create a function that takes 
+in a list and returns a list with the reverse order.*/
+
+function reverseList(list) {
+    let reversed = list.reverse();
+    return reversed;
+}
